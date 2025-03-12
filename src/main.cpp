58490a9d1f9deb12edb9ff4b4c0eb061ae669cf6
TakuOgawa <ogawtaku@gmail.com>
2025-03-13 01:00:34 +0900

    SSIDをスキャンして一覧を表示する

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@
 // 設定用のWebサーバー
 WebServer server(80);
 
-void displayWiFiInfo(); // Wi-Fi AP情報をLCDに表示
+void displayWiFiInfo(); // LCDにAP情報を表示
 void handleRoot();
+void handleScan();
 void handleSetWiFi();
 
 void setup()
@@ -20,7 +21,7 @@ void setup()
 
     // M5StackをWi-Fi APモードにする
     const char *apSSID = "M5Stack_Setup";
-    const char *apPassword = "12345678"; // 任意のパスワード（8文字以上）
+    const char *apPassword = "12345678"; // 任意のパスワード
     WiFi.softAP(apSSID, apPassword);
 
     // APモードのIPアドレスを取得
@@ -32,6 +33,7 @@ void setup()
 
     // Webサーバーのエンドポイントを設定
     server.on("/", handleRoot);
+    server.on("/scan", handleScan); // **SSIDスキャン結果を取得**
     server.on("/setwifi", HTTP_POST, handleSetWiFi);
     server.begin();
     Serial.println("Web Server started");
@@ -60,19 +62,51 @@ void displayWiFiInfo()
     M5.Lcd.print("http://192.168.4.1/");
 }
 
-// **Wi-Fi設定ページのHTML**
+// **Wi-Fi設定ページ（SSIDリストを取得可能）**
 void handleRoot()
 {
-    server.send(200,
-      "text/html",
+    server.send(200, "text/html",
                 "<html><body>"
                 "<h2>Wi-Fi Settings</h2>"
+                "<button onclick='scanWiFi()'>Scan Wi-Fi</button>"
+                "<ul id='ssidList'></ul>"
                 "<form action='/setwifi' method='POST'>"
-                "SSID: <input type='text' name='ssid'><br>"
+                "SSID: <input type='text' id='ssid' name='ssid'><br>"
                 "Password: <input type='password' name='password'><br>"
                 "<input type='submit' value='Connect'>"
                 "</form>"
+                "<script>"
+                "function scanWiFi() {"
+                " fetch('/scan').then(response => response.json()).then(data => {"
+                "   let list = document.getElementById('ssidList');"
+                "   list.innerHTML = '';"
+                "   data.forEach(ssid => {"
+                "     let item = document.createElement('li');"
+                "     item.innerHTML = `<a href='#' onclick='selectSSID(\"${ssid}\")'>${ssid}</a>`;"
+                "     list.appendChild(item);"
+                "   });"
+                " });"
+                "}"
+                "function selectSSID(ssid) {"
+                " document.getElementById('ssid').value = ssid;"
+                "}"
+                "</script>"
                 "</body></html>");
+}
+
+// **Wi-Fiスキャン結果をJSONで返す**
+void handleScan()
+{
+    int n = WiFi.scanNetworks();
+    String json = "[";
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0)
+            json += ",";
+        json += "\"" + WiFi.SSID(i) + "\"";
+    }
+    json += "]";
+    server.send(200, "application/json", json);
 }
 
 // **Wi-Fi接続設定を処理**
