c674c9ac704b16ce591d82f68ac39144b5b60b62
TakuOgawa <ogawtaku@gmail.com>
2025-03-13 00:54:59 +0900

    Wi-Fi APモードのM5Stackへの接続情報を表示する

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,69 @@
 #include <WebServer.h>
 #include <M5Stack.h>
 
+// 設定用のWebサーバー
 WebServer server(80);
 
+void displayWiFiInfo(); // Wi-Fi AP情報をLCDに表示
+void handleRoot();
+void handleSetWiFi();
+
+void setup()
+{
+    M5.begin();
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextColor(WHITE, BLACK);
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.print("Starting AP Mode...");
+
+    // M5StackをWi-Fi APモードにする
+    const char *apSSID = "M5Stack_Setup";
+    const char *apPassword = "12345678"; // 任意のパスワード（8文字以上）
+    WiFi.softAP(apSSID, apPassword);
+
+    // APモードのIPアドレスを取得
+    IPAddress apIP = WiFi.softAPIP();
+    Serial.printf("AP Mode: SSID=%s, IP=%s\n", apSSID, apIP.toString().c_str());
+
+    // LCDにWi-Fi情報を表示
+    displayWiFiInfo();
+
+    // Webサーバーのエンドポイントを設定
+    server.on("/", handleRoot);
+    server.on("/setwifi", HTTP_POST, handleSetWiFi);
+    server.begin();
+    Serial.println("Web Server started");
+}
+
+void loop()
+{
+    server.handleClient();
+}
+
+// **Wi-Fi接続情報をLCDに表示**
+void displayWiFiInfo()
+{
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.print("Wi-Fi Setup Mode");
+    M5.Lcd.setCursor(10, 40);
+    M5.Lcd.print("SSID: M5Stack_Setup");
+    M5.Lcd.setCursor(10, 60);
+    M5.Lcd.print("Password: 12345678");
+    M5.Lcd.setCursor(10, 90);
+    M5.Lcd.print("Connect to Wi-Fi and");
+    M5.Lcd.setCursor(10, 110);
+    M5.Lcd.print("open browser:");
+    M5.Lcd.setCursor(10, 140);
+    M5.Lcd.print("http://192.168.4.1/");
+}
+
+// **Wi-Fi設定ページのHTML**
 void handleRoot()
 {
-    server.send(200, "text/html",
+    server.send(200,
+      "text/html",
                 "<html><body>"
                 "<h2>Wi-Fi Settings</h2>"
                 "<form action='/setwifi' method='POST'>"
@@ -17,6 +75,7 @@ void handleRoot()
                 "</body></html>");
 }
 
+// **Wi-Fi接続設定を処理**
 void handleSetWiFi()
 {
     String ssid = server.arg("ssid");
@@ -24,36 +83,46 @@ void handleSetWiFi()
 
     if (ssid.length() > 0 && password.length() > 0)
     {
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.setCursor(10, 10);
+        M5.Lcd.print("Connecting to Wi-Fi...");
+        Serial.printf("Connecting to SSID: %s\n", ssid.c_str());
+
+        // Wi-Fi接続
+        WiFi.softAPdisconnect(true); // APモードを終了
         WiFi.begin(ssid.c_str(), password.c_str());
 
         int retry = 0;
         while (WiFi.status() != WL_CONNECTED && retry < 20)
         {
             delay(500);
+            Serial.print(".");
+            M5.Lcd.print(".");
             retry++;
         }
 
         if (WiFi.status() == WL_CONNECTED)
         {
+            Serial.println("\nConnected!");
+            Serial.print("IP Address: ");
+            Serial.println(WiFi.localIP());
+
+            M5.Lcd.fillScreen(BLACK);
+            M5.Lcd.setCursor(10, 10);
+            M5.Lcd.print("Wi-Fi Connected!");
+            M5.Lcd.setCursor(10, 40);
+            M5.Lcd.print(WiFi.localIP().toString());
+
             server.send(200, "text/html", "<h3>Connected! IP: " + WiFi.localIP().toString() + "</h3>");
         }
         else
         {
+            Serial.println("\nWi-Fi connection failed!");
+            M5.Lcd.fillScreen(BLACK);
+            M5.Lcd.setCursor(10, 10);
+            M5.Lcd.print("Wi-Fi Failed!");
+
             server.send(200, "text/html", "<h3>Connection Failed!</h3>");
         }
     }
-}
-
-void setup()
-{
-    M5.begin();
-    WiFi.softAP("M5Stack_Setup"); // M5StackをWi-Fi APにする
-    server.on("/", handleRoot);
-    server.on("/setwifi", HTTP_POST, handleSetWiFi);
-    server.begin();
-}
-
-void loop()
-{
-    server.handleClient();
 }
